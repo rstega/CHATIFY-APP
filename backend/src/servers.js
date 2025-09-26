9adc0c3e73946aed21495c76b42480d2c41cfccd
rstey <rstega780@gmail.com>
2025-09-26 18:06:22 +0300

    done

--- backend/src/servers.js
+++ backend/src/servers.js
@@ -3,10 +3,12 @@ import router from './routers/auth.router.js';
 import messageRouter from './routers/messege.Router.js';
 import path from 'path';
 const app = express();
+const __dirname = path.resolve();
 const port =process.env.port||3000;
 app.use('/login', router );
 app.use('/message', messageRouter);
-const __dirname = path.resolve();
+if (process.env.NODE_ENV === 'production')
+
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 app.get('*', (req , res) => {
     res.sendFile(path.join(__dirname, '../frontend','dist', 'index.html'));
