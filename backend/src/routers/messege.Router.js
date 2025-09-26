f6ce8cf9dff77e9137ececb2986d9a82573a137c
rstey <rstega780@gmail.com>
2025-09-26 11:05:17 +0300

    init message

--- backend/src/routers/messege.Router.js
+++ backend/src/routers/messege.Router.js
@@ -2,7 +2,7 @@ import express from "express";
 const messageRouter = express.Router();
 
 messageRouter.get("/", (req, res) => {
-  res.send("Message routeit is working");
+  res.send("Message routeit is working shoryly ");
 });
 
 export default messageRouter;
